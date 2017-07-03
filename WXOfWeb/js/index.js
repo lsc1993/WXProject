7cd26334336f69a2531802013446c0ecd43ea151
liushuang <liushuanggo@gmail.com>
2017-07-03 22:58:35 +0800

    提交index代码

---
+++ WXOfWeb/js/index.js
@@ -0,0 +1,15 @@
+$(function(){
+	
+})
+
+function tabClick(index) {
+	if (index == 1) {
+		$("#tab-1").addClass("tab-choosed").siblings().removeClass("tab-choosed");
+	} else if(index == 2) {
+		$("#tab-2").addClass("tab-choosed").siblings().removeClass("tab-choosed");
+	} else if(index == 3) {
+		$("#tab-3").addClass("tab-choosed").siblings().removeClass("tab-choosed");
+	} else if(index == 4) {
+		$("#tab-4").addClass("tab-choosed").siblings().removeClass("tab-choosed");
+	}
+}
