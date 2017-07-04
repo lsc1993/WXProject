b3deb5b8b2481cf9348a5339d69c542f8f97a78e
liushuang <liushuanggo@gmail.com>
2017-07-04 22:31:30 +0800

    提交产品vue组件

--- WXOfWeb/js/index.js
+++ WXOfWeb/js/index.js
@@ -13,3 +13,18 @@ function tabClick(index) {
 		$("#tab-4").addClass("tab-choosed").siblings().removeClass("tab-choosed");
 	}
 }
+
+var productWrapper = new Vue({
+	el: "#product-wrapper",
+	data: {
+		items: [
+		    {img: "../img/20172001.jpg"},
+		    {img: "../img/20172001.jpg"}
+		]
+	}
+})
+
+Vue.component("product-li",{
+	template: '<li><a><img src="{{imgUrl}}"></a></li>',
+	props:['imgUrl']
+})
