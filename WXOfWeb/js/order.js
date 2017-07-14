4202c7db871e08e62416a38d198d13f2bfc29c4d
lsc1993 <liushuanggo@gmail.com>
2017-07-14 19:02:56 +0800

    编写订单页面逻辑

--- WXOfWeb/js/order.js
+++ WXOfWeb/js/order.js
@@ -10,3 +10,33 @@ function autoHeightTextaera() {
 		$("#buy-message").animate({height:"30px"},300);	
 	});
 }
+
+var orderPage = new Vue({
+	el: "#order-page",
+	data: {
+		productMessage: 
+		    {imgurl: "../img/20172001.jpg",
+		     name: "change",
+		     standard: "4斤",
+		     price: "299.90",
+		     count: "1",
+		     deliveryCost: "0.00",
+		    }
+		
+	},
+	computed: {
+		totalCost: function() {
+			return parseFloat(this.price) + parseFloat(this.deliveryCost);
+		}
+	},
+	methods: {
+		submitOrder: function() {
+			
+		}
+	}
+})
+
+var chooseAddressWindow = Vue.component("chooseAddressWindow",{
+	props: ['addressItem'],
+	template: "#popup-window-address-choose"
+})
