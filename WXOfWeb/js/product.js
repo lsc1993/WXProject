f31e89be0ad99ae0c7f2a8a5689e722c669b9820
lsc1993 <liushuanggo@gmail.com>
2017-07-26 19:07:33 +0800

    补充页面交互逻辑

--- WXOfWeb/js/product.js
+++ WXOfWeb/js/product.js
@@ -15,6 +15,14 @@ var productPage = new Vue({
 		isShowAppraise: false,
 		isDetail: true,
 		isAppraise: false,
+		productMessage: {
+			productId: "20172648",
+			name: "ddd",
+			price: "199.00",
+			describe: "发文件哦忘记佛我",
+			delivery: "免运费",
+			saleVolume: 668
+		},
 		imgurls: [
 		    {imgurl : "../img/20172001.jpg"},
 		    {imgurl : "../img/20172001.jpg"},
@@ -59,6 +67,9 @@ var productPage = new Vue({
     	},
     	showPopupWindow: function(){
     		popup.showPopupWindow();
+    	},
+    	gotoShopCart: function() {
+    		window.location.href = "shop-cart.html"; 
     	}
     }
 })
@@ -74,8 +85,10 @@ var popup = new Vue({
 		isShow : false,
 		productMessage: {
 			imgurl: "../img/20172001.jpg",
+			productId: "20172648",
 		    name: "wow",
 		    price: "299",
+		    count: 1,
 		    labels: [
 		        {label: "weqwdqwdrtyrtqwdq", isChoosed: false},
 		        {label: "qweqwqweqyerte", isChoosed: false},
@@ -89,21 +102,93 @@ var popup = new Vue({
 		},
 		dismiss: function(){
 			this.isShow = false;
+		},
+		chooseLabel: function(index){
+			var length = this.productMessage.labels.length;
+			for(var i = 0;i < length;++i){
+				if(i == index){
+					this.productMessage.labels[i].isChoosed = true;
+					var a=this.productMessage.labels[i];
+					this.productMessage.labels.splice(i,1,a)
+				} else {
+					this.productMessage.labels[i].isChoosed = false;
+					var a=this.productMessage.labels[i];
+					this.productMessage.labels.splice(i,1,a)
+				}
+			}	
 		}
 	}
 })
 
-function chooseLabel(index){
-	var length = popup.productMessage.labels.length;
-	for(var i = 0;i < length;++i){
-		if(i == index){
-			popup.productMessage.labels[i].isChoosed = true;
-			var a=popup.productMessage.labels[i];
-			popup.productMessage.labels.splice(i,1,a)
-		} else {
-			popup.productMessage.labels[i].isChoosed = false;
-			var a=popup.productMessage.labels[i];
-			popup.productMessage.labels.splice(i,1,a)
+var dialogTip = Vue.component("dialog-tip",{
+	props: ['message'],
+	template: "#dialog-modal"
+})
+
+var dialog = new Vue({
+	el: "#dialog-div",
+	data: {
+		isShowDialog: false,
+		message: "请选择产品规格"
+	},
+	methods: {
+		dismiss: function(){
+			this.isShowDialog = false;
+		},
+		showDialog: function(){
+			this.isShowDialog = true;
 		}
-	}	
+	}
+})
+
+function addCount(){
+	if(popup.productMessage.count < 100){
+		popup.productMessage.count++;
+	} else {
+		alert();
+	}
+	
+}
+
+function subCount(){
+	if(popup.productMessage.count > 1){
+		popup.productMessage.count--;
+	}
+}
+
+function addShopCart(){
+    var length = popup.productMessage.labels.length;
+    var index = -1;
+    var standard = "";
+    for(var i = 0;i < length;++i){
+    	if(popup.productMessage.labels[i].isChoosed){
+    		index = i;
+    		standard = popup.productMessage.labels[i].label;
+    	}
+    }
+    if(index == -1 || standard == ""){
+  		dialog.showDialog();
+    }
+}
+
+function buyNow(){
+	var length = popup.productMessage.labels.length;
+    var index = -1;
+    var standard = "";
+    for(var i = 0;i < length;++i){
+    	if(popup.productMessage.labels[i].isChoosed){
+    		index = i;
+    		standard = popup.productMessage.labels[i].label;
+    	}
+    }
+    if(index == -1 || standard == ""){
+  		dialog.showDialog();
+    }else{
+    	var url = "order.html?"; 
+    	var param = "pid=" + popup.productMessage.productId
+    	+ "|count=" + popup.productMessage.count
+    	+ "|standard=" + standard;
+    	var encodeParam = window.btoa(param);
+    	window.location.href = url + encodeParam;
+    }
 }
